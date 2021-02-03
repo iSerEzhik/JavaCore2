97ccfe2330be3c8ad0b2a15cf81b956bdfd6965b
iSerEzhik <kretov67@inbox.ru>
2021-02-03 14:13:12 +0300

    HW2

--- src/lesson2/exceptions/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package lesson2.exceptions;
-
-public class MyArrayDataException extends IllegalArgumentException{
-    public MyArrayDataException(String message){
-        super(message);
-    }
-}
